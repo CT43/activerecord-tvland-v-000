a2f1d91c835bb14e0fdf609a265111cc1d2412ad
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-26 01:37:38 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
-  def build_network(:name = "Firefly")
+  def build_network(:call_letters = "Fox")
     Show.create(:name = :name)
   end
 
