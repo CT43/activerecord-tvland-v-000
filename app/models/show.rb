e373a038051ebdb6b57719292a191a22641bd2ad
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-26 01:42:18 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,9 +3,9 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def build_network(:call_letters = "Fox")
-    Network.create do |s|
-      s.name = name
-      s.save
+    Network.create do |n|
+      n.name = name
+      n.save
     end
   end
 
